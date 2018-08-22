05339705e37c82e458886851fc2228fa9f8561f4
jusagi <bqa1912@gmail.com>
2018-08-22 14:36:59 +0700

    My new symfony project

---
+++ Symfony/the_spacebar/src/Service/MarkdownHelper.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Service;
+
+use Michelf\MarkdownInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
+
+class MarkdownHelper
+{
+    private $cache;
+    private $markdown;
+    private $logger;
+
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $logger)
+    {
+        $this->cache = $cache;
+        $this->markdown = $markdown;
+        $this->logger = $logger;
+    }
+
+    public function parse(string $source): string
+    {
+        if (stripos($source, 'bacon') == true) {
+            $this->logger->info('They are talking about bacon again!');
+        }
+        else {
+            $this->logger->info('They did not say it!');
+        }
+
+        $item = $this->cache->getItem('markdown_' . md5($source));
+        if (!$item->isHit()) {
+            $item->set($this->markdown->transform($source));
+            $this->cache->save($item);
+        }
+
+        return $item->get();
+    }
+}
